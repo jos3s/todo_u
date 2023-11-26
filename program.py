afa4afabbfa5db66a7e3078fffca97eeb0eb7844
José Ulisses <julissesmacedo@outlook.com>
2023-11-25 21:44:45 -0300

    Estrutura do switch das ações

--- program.py
+++ program.py
@@ -1,2 +1,20 @@
+from modules.menu import menu
+from todo import *
+
 if __name__ == "__main__":
   print(f'Bem vindo')
+  while(True):
+    if option == 1:
+      pass
+    elif option == 2:
+      pass
+    elif option == 3:
+      pass
+    elif option == 4:
+      pass
+    elif option == 5:
+      pass
+    else:
+      break
+
+
