8661e9ca1786038b7127cebd8b8e5dea44a45917
José Ulisses <julissesmacedo@outlook.com>
2023-11-25 21:44:25 -0300

    Classe Todo

---
+++ todo.py
@@ -0,0 +1,26 @@
+class Todo:
+  def __init__(self, description):
+    self._description = description
+    self._done = False
+  
+  @property
+  def description(self):
+    return self._description
+
+  @description.setter
+  def description(self, description):
+    self._description = description
+
+  @property
+  def done(self):
+    return self._done
+
+  def done(self):
+    self._done = True
+
+  def undone(self):
+    self._done = False
+
+  def __str__(self):
+    return f"{self.description}, realizada: {'sim'if self.done  else 'não'}"
+
